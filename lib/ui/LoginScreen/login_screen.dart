441252b63fe22a1651ba6767c41904b898b781a1
Mario Espasa <73080627+Korah06@users.noreply.github.com>
2023-11-19 23:25:16 +0100

    base login made it

--- lib/ui/LoginScreen/login_screen.dart
+++ lib/ui/LoginScreen/login_screen.dart
@@ -6,26 +6,41 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [
-            Color.fromRGBO(234, 233, 233, 1),
-            Color.fromRGBO(180, 172, 180, 1),
-            Color.fromRGBO(153, 73, 73, 1),
-            Color.fromRGBO(43, 43, 43, 1)
-          ],
-        ),
-      ),
-      child: Column(
-        children: [
-          Center(
-            child: Image.asset('assets/logo.png'),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromRGBO(234, 233, 233, 1),
+                Color.fromRGBO(180, 172, 180, 1),
+                Color.fromRGBO(153, 73, 73, 1),
+                Color.fromRGBO(43, 43, 43, 1)
+              ],
+            ),
           ),
-        ],
-      ),
-    ));
+          child: Column(
+            children: [
+              Center(
+                child: Image.asset('assets/logo.png'),
+              ),
+              Transform.translate(
+                offset: const Offset(0, -50),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(120, 150, 87, 82),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 300,
+                  width: 320,
+                  child: Column(
+                    children: [TextFormField()],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
